545cd136a738ee93ad18eaaa214ea84300429e0e
vidya278 <vidyagowda278@gmail.com>
2022-07-12 22:09:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -37,7 +37,6 @@ resource "aws_security_group" "my_security_group" {
 # Create AWS ec2 instance
 resource "aws_instance" "myFirstInstance" {
   ami           = var.ami_id
-  key_name = var.key_name
   instance_type = var.instance_type
   security_groups= [var.security_group]
   tags= {
